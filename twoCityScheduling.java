f35d0aedf648c0c7b7b379694884df36fba65c6f
Ravi Kishan <raviksn2@gmail.com>
2020-06-04 23:50:12 +0530

    Create twoCityScheduling.java

---
+++ twoCityScheduling.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int twoCitySchedCost(int[][] costs) {
+        int len=costs.length;
+        int total=0;
+        int[] refund=new int[len];
+        for(int i=0;i<len;i++){
+            total+=costs[i][0];
+        }
+        for(int i=0;i<len;i++){
+            refund[i]=costs[i][1]-costs[i][0];
+        }
+        Arrays.sort(refund);
+        int sum=total;
+        for(int i=0;i<len/2;i++){
+            sum+=refund[i];
+        }
+        return sum;
+    }
+}
